df6795a840f149bc8c92649ad5369cb86d96e856
Nikhil Thota <nt92@ufl.edu>
2017-10-24 19:49:01 -0400

    refactored to constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,8 +1,10 @@
 public class Constants {
-    private final static String HANDSHAKE_HEADER = "P2PFILESHARINGPROJ";
-    private final static int NUM_ZERO_BYTE = 10;
-    private final static int HANDSHAKE_PAYLOAD_SIZE = 4;
-    private final static int MESSAGE_LENGTH_SIZE = 4;
-    private final static int MESSAGE_TYPE_SIZE = 1;
-    private final static String FILE_DIRECTORY_PREFIX = "project/peer_";
+    public static final String COMMON_CONFIG_FILENAME = "src/configs/Common.cfg";
+    public static final String PEER_INFO_FILENAME = "src/configs/PeerInfo.cfg";
+    public final static String HANDSHAKE_HEADER = "P2PFILESHARINGPROJ";
+    public final static int NUM_ZERO_BYTE = 10;
+    public final static int HANDSHAKE_PAYLOAD_SIZE = 4;
+    public final static int MESSAGE_LENGTH_SIZE = 4;
+    public final static int MESSAGE_TYPE_SIZE = 1;
+    public final static String FILE_DIRECTORY_PREFIX = "project/peer_";
 }
--- src/peerProcess.java
+++ src/peerProcess.java
@@ -5,18 +5,14 @@ import files.FileHandler;
 import java.util.List;
 
 public class peerProcess {
-
-    private static final String COMMON_CONFIG_FILENAME = "src/configs/Common.cfg";
-    private static final String PEER_INFO_FILENAME = "src/configs/PeerInfo.cfg";
-
     public static void main(String[] args) throws Exception {
         //Format of the process MUST have the peerID as an argument
         if (args.length != 1){
             System.out.println("Invalid Argument, must have peerID as the main argument");
         }
 
-        CommonConfig commonConfig = CommonConfig.createConfigFromFile(COMMON_CONFIG_FILENAME);
-        List<PeerInfoConfig> peerList = PeerInfoConfig.createPeerListFromFile(PEER_INFO_FILENAME);
+        CommonConfig commonConfig = CommonConfig.createConfigFromFile(Constants.COMMON_CONFIG_FILENAME);
+        List<PeerInfoConfig> peerList = PeerInfoConfig.createPeerListFromFile(Constants.PEER_INFO_FILENAME);
 
 //        int peerID = Integer.parseInt(args[0]);
 //        Peer peer = new Peer(peerID, commonConfig);
