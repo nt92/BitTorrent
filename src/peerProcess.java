3c9bf5e3cae127366123cf383fd1ffa28a32f892
Nikhil Thota <nt92@ufl.edu>
2017-10-24 12:02:40 -0400

    adding stuff to peerprocess

--- src/peerProcess.java
+++ src/peerProcess.java
@@ -1,6 +1,25 @@
+import configs.CommonConfig;
+import configs.PeerInfoConfig;
+
+import java.util.List;
+
+import static java.lang.System.exit;
+
 public class peerProcess {
 
-    public static void main(String[] args) {
+    private static final String COMMON_CONFIG_FILENAME = "configs/Common.cfg";
+    private static final String PEER_INFO_FILENAME = "configs/PeerInfo.cfg";
+
+    public static void main(String[] args) throws Exception {
+        //Format of the process MUST have the peerID as an argument
+        if (args.length != 1){
+            System.out.println("Invalid Argument, must have peerID as the main argument");
+            exit(1);
+        }
+
+        CommonConfig commonConfig = CommonConfig.createConfigFromFile(COMMON_CONFIG_FILENAME);
+        List<PeerInfoConfig> peerList = PeerInfoConfig.createPeerListFromFile(PEER_INFO_FILENAME);
+
 
     }
 }
